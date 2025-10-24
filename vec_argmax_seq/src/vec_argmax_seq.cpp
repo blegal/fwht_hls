9d85bbf3c476db062c04c602dd7cc1d48ef866a5
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-10-24 21:21:49 +0200

    first commit

---
+++ vec_argmax_seq/src/vec_argmax_seq.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+#define gf_size 64
+
+int argmax(const float value[gf_size])
+{
+    int    max_index = 0;
+    float  max_value = value[0];
+
+    for (int i = 1; i < gf_size; i++)
+    {
+        if (value[i] > max_value)
+        {
+            max_value = value[i];
+            max_index = i;
+        }
+    }
+    return max_index;
+}
